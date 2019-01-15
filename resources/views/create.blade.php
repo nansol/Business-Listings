d6c314c2075750913134163bc81f3c1228f08415
nansol <nancysgithub@gmail.com>
2019-01-15 11:44:24 +0100

    file updated

---
+++ resources/views/create.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><h4>Add Contact</h4></div>
+                <div class="card-body">
+                    <form action="{{action('ListingsController@store')}}" method="POST">
+                         <?= csrf_field() ?>
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" class="form-control" id="Name" name="name" placeholder="Company Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="website">Website</label>
+                            <input type="text" class="form-control" id="website" name="website" placeholder="Company Website">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control" id="email" name="email" placeholder="Contact Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">Phone Number</label>
+                            <input type="text" class="form-control" id="phone" name="phone" placeholder="Contact Phone Number">
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Address</label>
+                            <input type="text" class="form-control" id="address" name="address" placeholder="Business Address">
+                        </div>
+                        <div class="form-group">
+                            <label for="bio">Bio</label>
+                            <textarea class="form-control" id="bio" name="bio"placeholder="About the Business" rows="3"></textarea>
+                        </div>
+
+
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+@endsection
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="row">
+                        <div class="card-header col-10 align-self-start">Dashboard</div>
+                        <div class="card-header  col-2 align-self-end"><a href="/listings/create" class="badge badge-success">Add Listing</a></div>
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                   <h2 class="text-center">Your listings</h2>
+                    @if(count($listings))
+                    <div class="container">
+                            <div class="row">
+                                <div class="col-12">
+                                    <h3 class="">Company Name</h3>
+                                </div>
+                            </div>
+                                @foreach ($listings as $listing)
+                            <div class="row list-group list-group-flush mb-5">
+                                <div class="col-12">
+                                    <h5 class="list-group-item">{{$listing->name}}</h5>
+                                </div>
+                                <div class="col-12">
+                                    <h5 class="list-group-item">{{$listing->address}}</h5>
+                                </div>
+                            </div>
+                                @endforeach
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
